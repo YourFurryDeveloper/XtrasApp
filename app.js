3fa3b294d91fcf9096ce1a21c3b14beac88299cc
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-05-23 10:00:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -28,11 +28,33 @@ var app = new Framework7({
 
     ],
   });
-  
+
   var mainView = app.views.create('.view-main');
 
-  document.on('page:init', '.page[data-name="apps"]', function (e) {
-    $$('.google-download').on('click', function() {
-      console.log("google download");
+  function downloadWallpaper(url) {
+    // Create a hidden anchor element
+    const link = document.createElement('a');
+    link.href = url;
+  
+    // Try to extract a filename from the URL or default to 'wallpaper.jpg'
+    const urlParts = url.split('/');
+    let filename = urlParts[urlParts.length - 1].split('?')[0]; // get last part before query
+    if (!filename || filename.length < 5) filename = 'wallpaper.jpg';
+  
+    link.download = filename;
+  
+    // Append to body, click it, then remove
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  $$(document).on('page:init', '.page[data-name="apps"]', function (e) {
+    // Add click listener for wallpaper-download buttons inside this page
+    $$('.wallpaper-download').on('click', function (event) {
+      event.preventDefault();
+  
+      const imgSrc = $$(this).find('img').attr('src');
+      downloadWallpaper(imgSrc);
     });
   });  
