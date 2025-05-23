ce6dca1f550cdf792f0eb8b56590ad5db343221d
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-05-23 10:28:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-var $$ = Dom7;  // <-- add this
+var $$ = Dom7;
 
 var app = new Framework7({
   root: '#app',
-  el: '#app',
   name: 'Xtras',
   theme: 'ios',
   panel: { swipe: true },
@@ -10,7 +9,6 @@ var app = new Framework7({
   routes: [
     { path: '/apps/', url: 'apps.html' },
     { path: '/wallpapers/', url: 'wallpapers.html' },
-    { path: '/wallpaper1/', url: 'https://google.com' },
   ],
 });
 
@@ -19,16 +17,14 @@ var mainView = app.views.create('.view-main');
 function downloadWallpaper(url) {
   const link = document.createElement('a');
   link.href = url;
-  const urlParts = url.split('/');
-  let filename = urlParts[urlParts.length - 1].split('?')[0];
+  const parts = url.split('/');
+  let filename = parts[parts.length - 1].split('?')[0];
   if (!filename || filename.length < 5) filename = 'wallpaper.jpg';
   link.download = filename;
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
 }
-
-var $$ = Dom7;
 
 $$(document).on('click', '.page[data-name="wallpapers"] .wallpaper-download', function (e) {
   e.preventDefault();
